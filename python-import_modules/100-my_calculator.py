a12ffd94b197d01b2fa290d11c56a148287270fa
Elie <n.elie@alustudent.com>
2025-10-17 14:50:02 -0700

    Fixed 100-my_calculator

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,34 +1,24 @@
 #!/usr/bin/python3
-"""Calculator program that imports functions from calculator_1.py"""
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
 
-from calculator_1 import add, sub, mul, div
-import sys
-
-
-def main():
-    """Main function to handle calculator operations"""
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     a = int(sys.argv[1])
-    operator = sys.argv[2]
+    op = sys.argv[2]
     b = int(sys.argv[3])
-    
-    if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == chr(42):
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
+
+    if op == '+':
+        print(f"{a} {op} {b} = {add(a, b)}")
+    elif op == '-':
+        print(f"{a} {op} {b} = {sub(a, b)}")
+    elif op == '*':
+        print(f"{a} {op} {b} = {mul(a, b)}")
+    elif op == '/':
+        print(f"{a} {op} {b} = {div(a, b)}")
     else:
-        print("Unknown operator. Available operators: +, -, " + chr(42) + " and /")
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
-    
-    print("{} {} {} = {}".format(a, operator, b, result))
-
-
-if __name__ == "__main__":
-    main()
