c91a1ecf0b99a26d5e3d58e7ee475edd74ea787c
Elie <niyubwayoiraelie5777@gmail.com>
2025-10-30 12:19:49 +0200

    Fixed PEP8 error of W293 blank line contains whitespace in 12-roman_to_int.py file

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_values = {
         'I': 1,
         'V': 5,
@@ -12,21 +12,21 @@ def roman_to_int(roman_string):
         'D': 500,
         'M': 1000
     }
-    
+
     total = 0
     prev_value = 0
-    
+
     for char in reversed(roman_string):
         if char not in roman_values:
             return 0
-        
+
         current_value = roman_values[char]
-        
+
         if current_value < prev_value:
             total -= current_value
         else:
             total += current_value
-        
+
         prev_value = current_value
-    
+
     return total
