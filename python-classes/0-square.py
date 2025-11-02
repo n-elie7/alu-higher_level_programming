c78c6b3a30b544c0a4edeeea2d65a263a1eb2b11
Elie <niyubwayoiraelie5777@gmail.com>
2025-11-02 19:11:08 +0200

    Implemented 0-square which is the starting of classes in python.

---
+++ python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines an empty Square class.
+
+This module contains a simple Square class definition with no attributes
+or methods, serving as a basic template for future square implementations.
+"""
+
+
+class Square:
+    """An empty class that represents a square.
+    
+    This class currently has no attributes or methods but serves as
+    a foundation for defining square objects in future implementations.
+    """
+    pass
