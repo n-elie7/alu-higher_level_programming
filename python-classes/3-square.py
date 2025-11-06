e7f6f22dd9f7db9bad35facb55a6ad5a1bfa0724
Elie <niyubwayoiraelie5777@gmail.com>
2025-11-06 18:30:53 +0200

    Removed comments and docstrings from Square class definition

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-# Write a class Square that defines a square by: (based on 2-square.py)
+"""Module that defines a Square class with size."""
 
-# Private instance attribute: size
-# Instantiation with optional size: def __init__(self, size=0):
-# size must be an integer, otherwise raise a TypeError exception with the message size must be an integer
-# if size is less than 0, raise a ValueError exception with the message size must be >= 0
-# Public instance method: def area(self): that returns the current square area
-# You are not allowed to import any module
-# remember to add docstrings to your class and methods
+
 class Square:
     """A class that defines a square by its size."""
 
