475d72516067270a6a85d7a78c36fd131b91883a
Elie <n.elie@alustudent.com>
2025-11-11 23:15:58 +0200

    `Updated integer_validator method to check for boolean values`

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -10,7 +10,7 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """Function that validates integer"""
-        if not isinstance(value, int):
+        if type(value) is bool or type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
