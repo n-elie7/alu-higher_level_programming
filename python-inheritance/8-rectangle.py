4460f18674ccfa534fd479d39639dbd1175fa331
Elie <n.elie@alustudent.com>
2025-11-11 23:22:35 +0200

    Refactor Rectangle class documentation and improve formatting

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
-"""Module that defines the BaseGeometry class"""
-
-
-class BaseGeometry:
-    """BaseGeometry class"""
-    def area(self):
-        """Function that raise an exception"""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Function that validates integer"""
-        if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+"""Module for Rectangle class."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Reactangle class"""
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
+    """A rectangle class that inherits from BaseGeometry."""
 
+    def __init__(self, width, height):
+        """Initialize a Rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
