ea2d75d19baca869b43dd7bd60467af8887a605c
Elie <n.elie@alustudent.com>
2025-11-11 18:50:29 +0200

    Fix formatting by removing unnecessary blank line in Rectangle class constructor

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -21,7 +21,7 @@ class Rectangle(BaseGeometry):
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
-        
+
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
