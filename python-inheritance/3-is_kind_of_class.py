59e988bbb517bd97f0df2e6cb1d16f53373a462b
Elie <n.elie@alustudent.com>
2025-11-11 18:57:56 +0200

    Refactor documentation and add README for Python inheritance module

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,14 +4,14 @@
 
 def is_kind_of_class(obj, a_class):
     """Returns True if obj is an instance of, or inherited from, a_class
-    
+
     This function checks if the object is an instance of the specified class
     or an instance of a class that inherited from the specified class.
-    
+
     Args:
         obj: The object to check
         a_class: The class to compare against
-        
+
     Returns:
         bool: True if obj is an instance of a_class or its subclass,
               False otherwise
--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,15 +4,15 @@
 
 def inherits_from(obj, a_class):
     """Returns True if obj is an instance of a class that inherited from a_class
-    
+
     This function checks if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class.
     It returns False if the object is an exact instance of a_class.
-    
+
     Args:
         obj: The object to check
         a_class: The class to compare against
-        
+
     Returns:
         bool: True if obj's class inherited from a_class (but is not a_class),
               False otherwise
