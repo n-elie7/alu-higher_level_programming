50139f0dbad73d3e88b3e309f9c39b3805766f9a
Elie <n.elie@alustudent.com>
2025-11-22 13:26:07 +0200

    finished mandatory tasks

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,15 @@
+-- Convert the database to UTF8
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET = utf8mb4
+COLLATE = utf8mb4_unicode_ci;
+
+-- Convert the table first_table to UTF8
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+-- Ensure the 'name' column is UTF8
+ALTER TABLE first_table
+MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
