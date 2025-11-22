69b2bf1dc2fef795648e65da7395e77436385f85
Elie <n.elie@alustudent.com>
2025-11-22 13:41:47 +0200

    Simplified database and table conversion to UTF8

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,17 +1,5 @@
--- Convert the database to UTF8
-
 -- Convert database hbtn_0c_0 to UTF8
-ALTER DATABASE hbtn_0c_0
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Convert table first_table to UTF8
-ALTER TABLE hbtn_0c_0.first_table
-CONVERT TO CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
-
--- Convert field name in first_table to UTF8
-ALTER TABLE hbtn_0c_0.first_table
-MODIFY name VARCHAR(256)
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci;
+ALTER TABLE hbtn_0c_0.first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
