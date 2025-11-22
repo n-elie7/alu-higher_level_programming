220f2cdb7732522801f5d4c12ac345a8c76fec88
Elie <n.elie@alustudent.com>
2025-11-22 13:37:23 +0200

    Convert database and table to UTF8 character set and collation

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,16 +1,18 @@
 -- Convert the database to UTF8
 USE hbtn_0c_0;
 
+-- Convert database hbtn_0c_0 to UTF8
 ALTER DATABASE hbtn_0c_0
-CHARACTER SET = utf8mb4
-COLLATE = utf8mb4_unicode_ci;
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
 
--- Convert the table first_table to UTF8
-ALTER TABLE first_table
+-- Convert table first_table to UTF8
+ALTER TABLE hbtn_0c_0.first_table
 CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
 
--- Ensure the 'name' column is UTF8
-ALTER TABLE first_table
+-- Convert field name in first_table to UTF8
+ALTER TABLE hbtn_0c_0.first_table
 MODIFY name VARCHAR(256)
+CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
