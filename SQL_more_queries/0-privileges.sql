e2f4e8e7ed7679860a6e5a9f263e8ece0ebe11c9
Elie <n.elie@alustudent.com>
2025-11-22 18:09:24 +0200

    Added user creation and privilege management queries.

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,14 @@
+-- Privileges
+CREATE USER 'user_0d_1'@'localhost';
+
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+
+CREATE USER 'user_0d_2'@'localhost';
+
+CREATE DATABASE IF NOT EXISTS user_2_db;
+
+GRANT SELECT, INSERT ON user_2_db.* TO 'user_0d_2'@'localhost';
+
 -- List privileges for user_0d_1
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 
