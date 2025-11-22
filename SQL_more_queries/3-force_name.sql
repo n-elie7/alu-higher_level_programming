92482140c8824bd089e1817f11724b509a6a31d3
Elie <n.elie@alustudent.com>
2025-11-22 23:31:15 +0200

    database more queries.

---
+++ SQL_more_queries/3-force_name.sql
@@ -0,0 +1,7 @@
+-- Force name
+CREATE TABLE IF NOT EXISTS force_name (
+    id INT PRIMARY KEY
+    name VARCHAR(256) NOT NULL
+);
+
+
---
+++ SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,5 @@
+-- unique id table
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT UNIQUE DEFAULT 1
+    name VARCHAR(256)
+)
