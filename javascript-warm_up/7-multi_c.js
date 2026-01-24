7653fd2967652d8161852cd462a992aadc37ae00
Elie <n.elie@alustudent.com>
2026-01-24 15:23:08 +0200

    Fixed errors

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,12 +1,11 @@
 #!/usr/bin/node
-const args = process.argv.slice(2);
 
-const occurences = parseInt(args[0]);
+const x = Number(process.argv[2]);
 
-if (args === undefined) {
-    console.log('Missing number of occurrences');
-}
-
-for (let i = 0; i < occurences; i++) {
+if (!Number.isInteger(x)) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < x; i++) {
     console.log('C is fun');
+  }
 }
