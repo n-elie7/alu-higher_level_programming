7d127fbca9ab8927fde10876a4259a318803c275
Elie <n.elie@alustudent.com>
2026-01-28 15:57:35 +0200

    Fixed bug issues

--- javascript_objects_scopes_closures/2-rectangle.js
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -3,12 +3,13 @@
 class Rectangle {
   constructor(w, h) {
     if (
-        !Number.isInteger(w) 
-        || !Number.isInteger(h) 
-        || w <= 0 
-        || h <= 0) {
-          return {}
-        }
+      !Number.isInteger(w) || 
+      !Number.isInteger(h) || 
+      w <= 0 || 
+      h <= 0
+    ) {
+      return {}
+    }
 
     this.width = w,
     this.height = h
--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
 
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (
-        !Number.isInteger(w) 
-        || !Number.isInteger(h) 
-        || w <= 0 
-        || h <= 0) {
-          return {}
-        }
+      !Number.isInteger(w) || 
+      !Number.isInteger(h) || 
+      w <= 0 || 
+      h <= 0
+    ) {
+      return {}
+      }
 
     this.width = w,
     this.height = h
