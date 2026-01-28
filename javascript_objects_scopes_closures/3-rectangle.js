8c6446322894bc5f5d66335b5478dd7503a5dfb8
Elie <n.elie@alustudent.com>
2026-01-28 15:50:08 +0200

    printed square.

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -13,6 +13,16 @@ class Rectangle {
     this.width = w,
     this.height = h
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
 }
 
 module.exports = Rectangle;
