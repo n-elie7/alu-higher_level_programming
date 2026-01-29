20db5ecd98135830b9843062a06b073e122e3fe8
Elie <n.elie@alustudent.com>
2026-01-29 14:23:08 +0200

    solved 8th puzzle

---
+++ javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+exports.esrever = function (list) {
+  let reversed_list = []
+
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversed_list.push(list[i]);
+  }
+
+  return reversed_list;
+}
