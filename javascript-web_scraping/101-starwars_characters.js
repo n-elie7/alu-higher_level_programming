7f57c9e2d276a31d57a713155d286c93f6f9b2b2
Elie <n.elie@alustudent.com>
2026-02-03 12:54:40 +0200

    fixed bug about styling

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -6,7 +6,7 @@ const movieId = process.argv[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 // Wrap request in a Promise to use async/await
-function fetch(url) {
+function fetch (url) {
   return new Promise((resolve, reject) => {
     request.get(url, (error, response, body) => {
       if (error) {
@@ -18,7 +18,7 @@ function fetch(url) {
   });
 }
 
-async function printCharacters() {
+async function printCharacters () {
   try {
     const movie = await fetch(url);
     const characters = movie.characters; // array of URLs
